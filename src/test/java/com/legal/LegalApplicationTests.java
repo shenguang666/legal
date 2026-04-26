19127750dd2ca6b9cab3a06cfb4c6a72204d3265
Admin <1452980871@qq.com>
2026-04-26 23:30:47 +0800

    init

---
+++ src/test/java/com/legal/LegalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
